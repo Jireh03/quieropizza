868ec2de76196fb9f40584dc90fdc2de8c4b5f50
Jireh Tejeda <102043841+Jireh03@users.noreply.github.com>
2022-10-19 11:39:46 -0600

    Add files via upload

---
+++ Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+   public class Contexto: Context
+    {
+        public Contexto(): base("QuieropizzaDB")
+        {
+                
+        }
+        public Dbset<Producto> Productos { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quieropizza.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ProductosBL = new ProductosBL();
+            var listaProductos = ProductosBL.ObtenerProductos();
+            foreach (var Productos in listadeProductos)
+        }
+        MessageBox.Show(Producto.Descripcion )
+    }
+}
